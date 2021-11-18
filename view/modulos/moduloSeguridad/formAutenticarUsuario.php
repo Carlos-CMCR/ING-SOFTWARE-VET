12e4aa4beb3632d3c7342991f1954b2a9c391284
PuntitoCCR <carloscarbajalrojas@hotmail.com>
2021-11-17 21:14:41 -0500

    bugs

--- view/modulos/moduloSeguridad/formAutenticarUsuario.php
+++ view/modulos/moduloSeguridad/formAutenticarUsuario.php
@@ -26,6 +26,7 @@
     <div class="card mt-5 " style="width: 18rem;">
         <div class="card-header d-flex justify-content-center "><h5 class="card-title">Iniciar Sesión</h5></div>
         <div class="card-body">
+        <form action="../../../controller/getUsuario.php" method="POST">
         <div class="mb-3">
         <div class="row ">
             <div class="col-4 d-flex align-items-center"><label  for="usuario" class="form-label">Usuario</label></div>
@@ -42,6 +43,7 @@
         <div class="row">
                 <div class="col-12 d-flex justify-content-center"><input type="submit" class="botons"  name="btnIngresar" id="btnIngresar" value="Ingresar"></div>
         </div></div>
+        </form>
         <div class="row ">
             <a href="formVerificarUsuario.php" class="d-flex justify-content-center" id="links">Recordar Contraseña</a></div>
         </div>
