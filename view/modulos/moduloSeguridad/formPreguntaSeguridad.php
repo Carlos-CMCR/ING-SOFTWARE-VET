139f6cd02c33105122cfdeed0aeac5070570afa4
PuntitoCCR <carloscarbajalrojas@hotmail.com>
2021-11-17 21:36:51 -0500

    Recuperar contraseña 1 - sin estilos

--- view/modulos/moduloSeguridad/formPreguntaSeguridad.php
+++ view/modulos/moduloSeguridad/formPreguntaSeguridad.php
@@ -20,7 +20,7 @@
                  </div>
             </div>
         </div>
-    </nav>
+</nav>
     
     <div class="d-flex justify-content-center">               
     <div class="card mt-5 " style="width: 18rem;">
@@ -29,25 +29,24 @@
         <form action="getRespuesta.php" method="POST">
         <div class="mb-3">
         <div class="row ">
-            <div class="col-4 d-flex align-items-center"><label  for="usuario" class="form-label">Usuario</label></div>
-            <div class="col-8 "><input type="text" class="form-control" aria-describedby="text"  name="usuario" id="usuario"> </div>
+            <div class="col-12 d-flex justify-content-center"><label  for="usuario" class="form-label">¿Cuál es la palabra clave?</label></div>
         </div>
         </div>
-        
-
+        <div class="row ">
+        <div class="col-12 d-flex align-items-center"><input type="text" class="form-control" aria-describedby="text"  value="" name="respuesta" id="respuesta"> </div>
+        </div>
+        </div>
         <div class="mb-2">
         <div class="row">
-                <div class="col-12 d-flex justify-content-center"><input type="submit" class="botons"  name="btnBuscar" id="btnBuscar" value="Buscar"></div>
+                <div class="col-12 d-flex justify-content-center"><input type="submit" class="botons"  name="btnEnviar" id="btnEnviar" value="Enviar"></div>
         </div></div>
         </form>
-        <div class="row ">
-            <a href="formAutenticarUsuario.php" class="d-flex justify-content-center" id="links">Atras</a></div>
-        </div>
+    
+    </div>
         
     </div>
-    </div>
-</div> 
-</div> 
+    
+
 </body>
 </html>
 
--- view/modulos/moduloSeguridad/formVerificarUsuario.php
+++ view/modulos/moduloSeguridad/formVerificarUsuario.php
@@ -1,7 +1,4 @@
 
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -50,8 +47,7 @@
         
     </div>
     </div>
-</div> 
-</div> 
+
 </body>
 </html>
 
@@ -60,36 +56,7 @@
 
 
 
-<!--
 
-<!DOCTYPE html>
-              <html lang="en">
-              <head>
-                  <meta charset="UTF-8">
-                  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-                  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-                  <link rel="stylesheet" href="../estilos.css">
-                  <title>Recuperar contraseña</title>
-              </head>
-              <body class="login">      
-                    
-                    <div  class="card bg-light mb-3" style="max-width: 20rem;">
-                        <div class="card-header"><h5 class="card-title">Recuperar contraseña</h5></div>
-                            <div class="card-body">
-                                <form action="../../../controller/getLogin.php" method="POST">
-                                    <p>Usuario:</p>
-                                        <input type="text" name="usuario" id="usuario"><br/><br/>
-                                        <div class="button-center"> <input type="submit" class="btn btn-primary" name="btnBuscar" id="btnBuscar" value="Buscar"></div>
-                                    
-                                </form>
-                                <a href="formAutenticarUsuario.php">Atras</a>
-
-                        </div>
-                    </div>
-              </body>
-              </html>
-                    
                          
  
 
