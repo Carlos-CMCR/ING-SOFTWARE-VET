c44699e8f087fe3223067095d335dc9e3b8571d9
2016100129 <jorgechavez199524@gmail.com>
2021-11-18 10:45:58 -0500

    as

--- view/modulos/moduloUsuario/formUsuarioPerfil.php
+++ view/modulos/moduloUsuario/formUsuarioPerfil.php
@@ -18,7 +18,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <script src="https://kit.fontawesome.com/9cdc4a056d.js" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="../../../styles/stylesLogin.css">
+    <link rel="stylesheet" href="../styles/stylesLogin.css">
 </head>
 <body>
     <nav class="menu navbar navbar-expand-lg navbar-light">
