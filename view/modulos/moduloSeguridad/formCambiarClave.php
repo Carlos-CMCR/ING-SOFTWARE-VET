d2a754955bc9cdb7e36dd39c6f1bdc8fa30df62b
PuntitoCCR <carloscarbajalrojas@hotmail.com>
2021-11-18 16:17:40 -0500

    estilos

--- view/modulos/moduloSeguridad/formCambiarClave.php
+++ view/modulos/moduloSeguridad/formCambiarClave.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="../../../styles/stylesLogin.css">
+    <link rel="stylesheet" href="../styles/stylesLogin.css">
     <title>Recuperar contraseña</title>
 </head>
 <body class="background-acceso">
@@ -14,7 +14,7 @@
 
 <nav class="menu navbar navbar-expand-lg navbar-light">
         <div class="container">      
-                    <a href=""><img src="../../img/logo.png" alt=""></a>
+                    <a href=""><img src="../view/img/logo.png" alt=""></a>
             <div class="collapse navbar-collapse">
                  <div class="navbar-nav ms-auto">
                  <a href="../../../index.php" class="nav">HOME</a>
